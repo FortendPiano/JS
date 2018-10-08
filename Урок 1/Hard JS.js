3792f453b31a7c67937c3774e07654e6c8a84476
Andrey <andrey_y4in_1297@bk.ru>
2018-10-08 19:00:37 +0400

    Это мой третий коммит

---
+++ Урок 1/Hard JS.js
@@ -0,0 +1,7 @@
+'use strict'; 
+var num = 33721;
+
+var proizv = console.log(3*3*7*2*1);
+    console.log(proizv^3);
+console.log(proizv(0,1));
+
--- Урок 1/JS.js
+++ Урок 1/JS.js
@@ -3,17 +3,22 @@
 var money = prompt("Ваш бюджет на месяц?"),
     time = prompt("Введите дату в формате YYYY-MM-DD"),
     oneQestion = prompt("Введите обязательную статью расходов в этом месяце"),
-    twoQestion = prompt("Во сколько обойдется?");
+    twoQestion = prompt("Во сколько обойдется?"),
+    threeQestion = prompt("Введите обязательную статью расходов в этом месяце"),
+    forQestion = prompt("Во сколько обойдется?");
+
 
 var appData = {
     money,
     timeData: time,
     expenses: {
         oneQestion: twoQestion,
+        threeQestion: forQestion,
     },
     optionalExpenses: [],
     income: [],
     savings: false,
 
 };
+console.log(appData.expenses);
 console.log((money-twoQestion)/30);
